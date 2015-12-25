4c80c0a7d7fa04e6ec2b9a77f43dac747ff863dd
David Whale <david@thinkingbinaries.com>
2015-12-25 23:38:52 +0000

    Started to flesh out parser architecture

--- src/forth.py
+++ src/forth.py
@@ -5,32 +5,90 @@
 # Inspired by:
 # http://angg.twu.net/miniforth-article.html
 
-# stack
-# has:
-#   push, pop, top
+import re
 
-# inner interpreter - will interpret bytecodes
-# has:
-#   dictionary (of words, primitive and non primitive)
-#   return stack
-#   data stack
-#   memory?? (for variables?)
-#   next (run next bytecode in bytecode list)
 
-# outer interpreter - will interpret text
-# has:
-#   parser:
-#     line being parsed
-#     pos on that line of next word
-#     parsers: pattern, spaces, word, newline, restofline, wordornewline
-#     getword, getwordornewline
-#   interpreter:
-#     interpretprimitive, interpretnonprimitive, interpretnumber
+#----- STACK ------------------------------------------------------------------
+
+class Stack():
+    def size(self):
+        pass
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def top(self):
+        pass
+
+    def __setattr__(self, key, value):
+        pass
+
+    def __getattr__(self, item):
+        pass
+
+
+#----- INNER BYTECODE INTERPRETER ---------------------------------------------
+
+class Inner():
+    dict = Stack()
+    rs   = Stack()
+    ds   = Stack()
+    memory = []
+
+    def next(self):
+        pass
+
+
+#----- OUTER TEXT INTERPRETER -------------------------------------------------
+
+class Parser():
+    pos = 0
+    line = ""
+
+    def next(self):
+        pass
+
+    def parsebypattern(self, pat):
+        capture, newpos = re.match(self.line, pat, self.pos)
+        if newpos:
+            self.pos = newpos;
+            return capture
+
+    def parsespaces(self):
+        return self.parsebypattern("^([ \t]*)()")
+
+    def parseword(self):
+        return self.parsebypattern("^([^ \t\n]+)()")
+
+    def parsenewline(self):
+        return self.parsebypattern("^(\n)()")
+
+    def parserestofline(self):
+        return self.parsebypattern("^([^\n]*)()")
+
+    def parsewordornewline(self):
+        return self.parseword() or self.parsenewline()
+
+    def getword(self):
+        self.parsespaces()
+        return self.parseword()
+
+    def getwordornewline(self):
+        self.parsespaces()
+        return self.parsewordornewline()
+
+
+#----- FORTH LANGUAGE ---------------------------------------------------------
 
 class Forth:
     INTERPRET = "interpret"
     STOP      = "stop"
-
+    HEAD      = "head"
+    LIT       = "lit"
+    FORTH     = "forth"
 
     mode      = INTERPRET
 
@@ -43,11 +101,17 @@ class Forth:
         print("will run interpreter here")
         self.mode = self.STOP
 
-    #other modes to write
-    #def head(self):
-    #def forth(self):
-    #def lit(self):
+    def head(self):
+        print("unimplemented mode:head")
 
+    def forth(self):
+        print("unimplemented mode:forth")
+
+    def lit(self):
+        print("unimplemented mode:lit")
+
+
+#----- RUNNER -----------------------------------------------------------------
 
 def run():
     f = Forth()

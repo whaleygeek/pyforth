2f797da036e3037885df8cb95ce8191158050921
David Whale <david@thinkingbinaries.com>
2015-12-25 10:58:35 +0000

    Minimal language with just stop mode added

---
+++ src/forth.py
@@ -0,0 +1,32 @@
+# forth.py  25/12/2015  D.J.Whale
+#
+# An experiment to write a minimal FORTH language in Python
+#
+# Inspired by:
+# http://angg.twu.net/miniforth-article.html
+
+class FinishedException(Exception):
+    pass
+
+class Mode:
+    current = "stop"
+
+    def stop(self):
+        print("stopping")
+        raise FinishedException
+
+
+def run():
+    mode = Mode()
+    while True:
+        method = getattr(mode, mode.current)
+        method()
+
+
+if __name__ == "__main__":
+    try:
+        run()
+    except FinishedException:
+        pass
+
+# END

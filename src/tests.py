e21a092d5f89befb8563963533d6be4bb8f408d0
David Whale <david@thinkingbinaries.com>
2016-01-06 16:41:11 +0000

    DOLIT now explicit in tests

--- src/tests.py
+++ src/tests.py
@@ -15,86 +15,87 @@ class TestForth(unittest.TestCase):
     def test_01_star(self):
         """Output a single * on stdout"""
 
-        self.f.create_word("TEST", 42, "EMIT") #TODO: "DOLIT", 42
+        self.f.create_word("TEST", " DOLIT", 42, "EMIT")
         self.f.execute_word("TEST")
 
-    def Xtest_02_hello(self):
+    def test_02_hello(self):
         """Output a Hello world! message"""
 
         msg = "Hello world!\n"
         pfa = []
         for ch in msg:
-            pfa.append(ord(ch)) #TODO: "DOLIT" ord(ch)
+            pfa.append(" DOLIT")
+            pfa.append(ord(ch))
             pfa.append("EMIT")
 
         self.f.create_word("TEST", *pfa)
         self.f.execute_word("TEST")
 
-    def Xtest_03_add(self):
+    def test_03_add(self):
         """Add 1 and 2"""
-        self.f.create_word("TEST", 1, 2, "+", ".") #TODO: DOLIT, 1, DOLIT, 2, + .
+        self.f.create_word("TEST", " DOLIT", 1, " DOLIT", 2, "+", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_04_sub(self):
+    def test_04_sub(self):
         """Subtract"""
-        self.f.create_word("TEST", 2, 1, "-", ".")
+        self.f.create_word("TEST", " DOLIT", 2, " DOLIT", 1, "-", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_05_and(self):
-        self.f.create_word("TEST", 0xFFFF, 0x8000, "AND", ".") #TODO: DOLIT, 0xFFFF, DOLIT, 0x8000, AND
+    def test_05_and(self):
+        self.f.create_word("TEST", " DOLIT", 0xFFFF, " DOLIT", 0x8000, "AND", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_06_or(self):
-        self.f.create_word("TEST", 0xFFFF, 0x8000, "OR", ".") #TODO
+    def test_06_or(self):
+        self.f.create_word("TEST", " DOLIT", 0xFFFF, " DOLIT", 0x8000, "OR", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_07_xor(self):
-        self.f.create_word("TEST", 0x0001, 0x8000, "XOR", ".") #TODO
+    def test_07_xor(self):
+        self.f.create_word("TEST", " DOLIT", 0x0001, " DOLIT", 0x8000, "XOR", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_08_mult(self):
-        self.f.create_word("TEST", 2, 4, "*", ".") # TODO
+    def test_08_mult(self):
+        self.f.create_word("TEST", " DOLIT", 2, " DOLIT", 4, "*", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_09_div(self):
-        self.f.create_word("TEST", 10, 3, "/", ".") # TODO
+    def test_09_div(self):
+        self.f.create_word("TEST", " DOLIT", 10, " DOLIT", 3, "/", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_10_mod(self):
-        self.f.create_word("TEST", 10, 3, "MOD", ".") #TODO
+    def test_10_mod(self):
+        self.f.create_word("TEST", " DOLIT", 10, " DOLIT", 3, "MOD", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_20_dot(self):
-        self.f.create_word("TEST", 10, 20, ".", ".") #TODO
+    def test_20_dot(self):
+        self.f.create_word("TEST", " DOLIT", 10, " DOLIT", 20, ".", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_21_swap(self):
-        self.f.create_word("TEST", 10, 20, "SWAP", ".", ".") #TODO
+    def test_21_swap(self):
+        self.f.create_word("TEST", " DOLIT", 10, " DOLIT", 20, "SWAP", ".", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_22_dup(self):
-        self.f.create_word("TEST", 10, "DUP", ".", ".") #TODO
+    def test_22_dup(self):
+        self.f.create_word("TEST", " DOLIT", 10, "DUP", ".", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_23_over(self):
-        self.f.create_word("TEST", 10, 20, "OVER", ".", ".", ".") #TODO
+    def test_23_over(self):
+        self.f.create_word("TEST", " DOLIT", 10, " DOLIT", 20, "OVER", ".", ".", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_24_rot(self):
-        self.f.create_word("TEST", 10, 20, 30, "ROT", ".", ".", ".") #TODO
+    def test_24_rot(self):
+        self.f.create_word("TEST", " DOLIT", 10, " DOLIT", 20, " DOLIT", 30, "ROT", ".", ".", ".")
         self.f.execute_word("TEST")
 
-    def Xtest_25_drop(self):
-        self.f.create_word("TEST", 10, 20, "DROP", ".", ".") #TODO
+    def test_25_drop(self):
+        self.f.create_word("TEST", " DOLIT", 10, " DOLIT", 20, "DROP", ".", ".")
         #TODO should be a 'data stack underflow' exception
         self.f.execute_word("TEST")
 
-    def Xtest_30_wblk_rblk(self):
+    def test_30_wblk_rblk(self):
         # wblk ( n a -- )  i.e. blocknum addr
         #self.f.machine.mem.dump(1024, 16)
 
-        self.f.create_word("W", 0, 1024, "WBLK") # probably DICT #TODO
-        self.f.create_word("R", 0, 65536-1024, "RBLK") #TODO
+        self.f.create_word("W", " DOLIT", 0, " DOLIT", 1024, "WBLK") # probably DICT
+        self.f.create_word("R", " DOLIT", 0, " DOLIT", 65536-1024, "RBLK")
         self.f.execute_word("W")
 
         # rblk ( n a -- )  i.e. blocknum addr
